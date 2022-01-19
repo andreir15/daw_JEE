93fe522644af9851e4c47d6e3faf1af8aa597b2a
andreir15 <90853700+andreir15@users.noreply.github.com>
2022-01-19 14:21:17 +0100

    Add files via upload

--- ej6.java
+++ ej6.java
@@ -9,33 +9,52 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet("/ej4")
-public class ej4 extends HttpServlet {
+/**
+ * Servlet implementation class ej6
+ */
+@WebServlet("/ej6")
+public class ej6 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-  
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String nombre=request.getParameter("nombre");
+		
 		response.setContentType("text/html");
 		PrintWriter pw=response.getWriter();
-				if(nombre==null) {
 	
 			pw.println("<form method=\"post\">");
 			pw.println("Nombre");
 			pw.println("<input type=\"text\" name=\"nombre\"> ");
 			pw.println("<br>");
+			pw.println("<h2>AFICIONES</h2>");
+			pw.println("Bailar<input type=\"checkbox\" value=\"bailar\" name=\"aficiones[]\">");
+			pw.println("<br>");
+			pw.println("Cantar<input type=\"checkbox\" value=\"cantar\" name=\"aficiones[]\">");
+			pw.println("<br>");
 			pw.println("<input type=\"submit\">");
 			pw.println("</form>");
-		}else {
-		response.getWriter().println("Hola "+nombre);
-	}
+		
+
 				
 	}
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String nombre=req.getParameter("nombre");
+		String aficiones[]=req.getParameterValues("aficiones[]");
 	   resp.setContentType("text/html");
 	   PrintWriter pw=resp.getWriter();
-	   pw.println("Hola "+nombre);
+	   if(nombre=="") {
+	   pw.println("<h1>Debes introducir un nombre</h1>");
+	   }else {
+		   if(aficiones==null) {
+			   pw.println("<h1>A "+nombre+" no le gusta nada</h1>");
+		   }
+		   else{
+			   
+			   pw.println("<h1>A "+nombre+" le gusta ");
+			   for(String aficion:aficiones){   
+		   pw.println(aficion);
+			   }
+		   }
+		   
+	   }
 	}
 }
