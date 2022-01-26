e336711fb94d082de7f2803c75eddac450001fa9
andreir15 <90853700+andreir15@users.noreply.github.com>
2022-01-26 11:01:24 +0100

    Add files via upload

--- EjemplosHibernate/src/hibernate/entities/Persona.java
+++ EjemplosHibernate/src/hibernate/entities/Persona.java
@@ -1,8 +1,40 @@
 package hibernate.entities;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Persona {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
 private String nombre;
+
+public Persona() {
+	this.nombre="Andrei";
+}
 public Persona(String nombre) {
 	this.nombre=nombre;
 }
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+
+@Override
+public String toString() {
+	return this.nombre;
+}
+
 }
